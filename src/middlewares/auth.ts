0a68ccc1f829dd030da5c9585506d960e8bda264
Jean <jeanvictorgithubprojects@gmail.com>
2023-02-06 22:45:14 -0300

    feat(middleware/auth) add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from 'express';
+import { verify } from 'jsonwebtoken';
+require('dotenv').config();
+
+type TokenPayload = {
+    id: string
+}
+
+export function AuthMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+){
+  const { authorization } = request.headers;
+
+  if(!authorization){
+    return response.status(401).json({error: 'Token not provided'});
+  }
+
+  const [,token] = authorization.split(' ');
+
+  try {
+    const SECRET_KEY_WEB_TOKEN = process.env.SECRET_KEY_WEB_TOKEN as string;
+    const decoded = verify(token, SECRET_KEY_WEB_TOKEN);
+    const { id } = decoded as TokenPayload;
+    request.userId = id;
+    
+    next();
+  }catch(error){
+    return response.status(401).json({error: 'Token invalid'});
+
+  }
+}
