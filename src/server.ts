e50f2141119617a1728b6a851949623be4d76fdb
Jean <jeanvictorgithubprojects@gmail.com>
2023-02-19 11:41:09 -0300

    docs: add swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'node:path';
 import { routes } from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json'; 
 
 require('dotenv').config();
 
@@ -9,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
+app.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => {
   console.log('Server is started on http://localhost:3333');
